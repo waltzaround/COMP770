c5115633252e08188535d6f2a90de033c54476f4
Walter Lim <vxs4050@autuni.ac.nz>
2016-03-26 15:10:03 +1300

    initial file upload

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Conveyor : MonoBehaviour {
+	Rigidbody body;
+	public float speed = 2.0f;
+
+	void FixedUpdate()
+	{
+		body = GetComponent<Rigidbody>();
+		body.position -= transform.forward * speed * Time.deltaTime;
+		body.MovePosition (body.position + transform.forward * speed * Time.deltaTime);
+
+	}
+
+}
+
---
+++ Assets/Scripts/addingforce.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class addingforce : MonoBehaviour 
+{
+	public float thrust;
+	public Rigidbody rb;
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() 
+	{
+		rb.AddForce(Vector3.forward * thrust);
+	}
+}
