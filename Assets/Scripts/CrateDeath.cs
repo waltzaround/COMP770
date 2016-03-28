01034c18babafb6c7e370625cf54b4147d03b4a4
Walter Lim <vxs4050@autuni.ac.nz>
2016-03-29 11:23:30 +1300

    Finished phase 1 sugar loading bay

---
+++ Assets/Scripts/CrateDeath.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//destroys a crate if it touches a endpoint
+
+public class CrateDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.collider.tag == "crateMuncher") 
+		{
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/examplespawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class examplespawn : MonoBehaviour
+{
+	public GameObject prefab;
+
+	void Start()
+	{
+			
+		InvokeRepeating("spawningsugar", 2, 0.3F);
+
+	}
+
+	void spawningsugar()
+	{
+		Instantiate(prefab, new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z), Quaternion.identity);
+
+	}
+
+
+
+}
