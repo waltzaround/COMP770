7cbd8bb611a443b385e15172c92e9f2c7b18b3b0
waltzaround <vxs4050@autuni.ac.nz>
2016-03-31 17:10:42 +1300

    small edits

---
+++ Assets/Scripts/lifter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//destroys a crate if it touches a endpoint
+
+public class lifter : MonoBehaviour
+{
+    public float thrust;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "lift")
+        {
+            rb.AddForce(Vector3.up * 70,ForceMode.Impulse);
+        }
+    }
+
+
+}
