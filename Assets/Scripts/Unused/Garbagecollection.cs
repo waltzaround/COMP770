c3182ff105880efa1979a3fe241120e86d9943ca
waltzaround <vxs4050@autuni.ac.nz>
2016-04-02 16:49:37 +1300

    Finished object loop, and started tidying code

---
+++ Assets/Scripts/Unused/Garbagecollection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Garbagecollection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void oncollisionenter(Collision otherObj)
+    {
+        if (otherObj.collider.tag == "sugar")
+        {
+            Destroy(otherObj.gameObject);
+        }
+    }
+
+}
