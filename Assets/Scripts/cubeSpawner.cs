bd50f952544341bb82a575cd67bd45685884b96f
waltzaround <vxs4050@autuni.ac.nz>
2016-04-04 10:36:27 +1200

    Liah's interim edits

---
+++ Assets/Scripts/cubeSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
